136d55862e68e8f37a2d525d7b4cef8dc5ec3827
Ollie Coulson <olliecoulson@Ollies-MacBook-Pro.local>
2015-11-09 18:25:55 +0000

    Wrote HashUtilities.java with single static method shortHash(int) which returns a number between 0 and 1000 when given a hash code of an object

---
+++ HashUtilities.java
@@ -0,0 +1,12 @@
+/**
+ * Lecture 8, Exercise 5
+ * HashUtilities class
+ */
+public class HashUtilities {
+	
+	public static int shortHash(int input) {
+		return Math.abs(input % 1000);
+		
+	}
+	
+}
